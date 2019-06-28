bae605b0766b2b87ceac65a117d65aa650bc5ff5
lyxcious <felix_laurensius@yahoo.com>
2019-06-28 09:25:57 +0700

    update snap g-vision

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <router-view></router-view>
+    <Snaps/>
   </v-app>
 </template>
 
@@ -10,13 +11,15 @@ import NavBar from "./components/NavBar"
 import RegisterButton from "./components/RegisterButton"
 import LoginButton from "./components/LoginButton"
 import { mapActions } from "vuex";
+import Snaps from './components/snapshot'
 
 export default {
   name: 'App',
   components: {
    NavBar,
    RegisterButton,
-   LoginButton
+   LoginButton,
+   Snaps
   },
   data () {
     return {
